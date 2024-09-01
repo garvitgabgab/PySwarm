2210b46d59d25a79baa9d4c7230d2a7612d11e7d
Garvit <gaurgarvit64@gmail.com>
2024-09-01 22:58:03 +0530

    staged

--- app/main.py
+++ app/main.py
@@ -1,47 +1,101 @@
+from app.tracker import Tracker
+from app.torrent import Torrent
+from app.peer import Peer
+from app import bencode
+from io import BytesIO
+import argparse
+import random
 import json
-import sys
 
-# import bencodepy - available if you need it!
-# import requests - available if you need it!
-
-# Examples:
-#
-# - decode_bencode(b"5:hello") -> b"hello"
-# - decode_bencode(b"10:hello12345") -> b"hello12345"
-def decode_bencode(bencoded_value):
-    if chr(bencoded_value[0]).isdigit():
-        first_colon_index = bencoded_value.find(b":")
-        if first_colon_index == -1:
-            raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index+1:]
-    else:
-        raise NotImplementedError("Only strings are supported at the moment")
+MY_PEER_ID = b'github.com/EpocDotFr'
 
 
-def main():
-    command = sys.argv[1]
+def _bytes_to_str(obj) -> str:
+    if isinstance(obj, bytes):
+        return obj.decode()
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    if command == "decode":
-        bencoded_value = sys.argv[2].encode()
-
-        # json.dumps() can't handle bytes, but bencoded "strings" need to be
-        # bytestrings since they might contain non utf-8 characters.
-        #
-        # Let's convert them to strings for printing to the console.
-        def bytes_to_str(data):
-            if isinstance(data, bytes):
-                return data.decode()
-
-            raise TypeError(f"Type not serializable: {type(data)}")
-
-        # Uncomment this block to pass the first stage
-        # print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
-    else:
-        raise NotImplementedError(f"Unknown command {command}")
+    raise TypeError()
 
 
-if __name__ == "__main__":
+def main() -> None:
+    arg_parser = argparse.ArgumentParser()
+
+    command_arg_parser = arg_parser.add_subparsers(dest='command', required=True)
+
+    decode_arg_parser = command_arg_parser.add_parser('decode')
+    decode_arg_parser.add_argument('data')
+
+    info_arg_parser = command_arg_parser.add_parser('info')
+    info_arg_parser.add_argument('filename')
+
+    peers_arg_parser = command_arg_parser.add_parser('peers')
+    peers_arg_parser.add_argument('filename')
+
+    handshake_arg_parser = command_arg_parser.add_parser('handshake')
+    handshake_arg_parser.add_argument('filename')
+    handshake_arg_parser.add_argument('address')
+
+    download_piece_arg_parser = command_arg_parser.add_parser('download_piece')
+    download_piece_arg_parser.add_argument('filename')
+    download_piece_arg_parser.add_argument('piece_index', type=int)
+    download_piece_arg_parser.add_argument('-o', '--output')
+
+    args = arg_parser.parse_args()
+
+    if args.command == 'decode':
+        with BytesIO(args.data.encode()) as f:
+            print(json.dumps(bencode.unpack(f), default=_bytes_to_str))
+    elif args.command == 'info':
+        with open(args.filename, 'rb') as f:
+            torrent = Torrent.load(f)
+
+        print(f'Tracker URL: {torrent.tracker_url}')
+        print(f'Length: {torrent.length}')
+        print(f'Info Hash: {torrent.info_hash.hexdigest()}')
+        print(f'Piece Length: {torrent.piece_length}')
+        print(f'Piece Hashes:')
+
+        for piece_hash in torrent.piece_hashes:
+            print(piece_hash)
+    elif args.command == 'peers':
+        with open(args.filename, 'rb') as f:
+            torrent = Torrent.load(f)
+
+        tracker = Tracker(MY_PEER_ID, torrent)
+
+        for peer in tracker.request()['peers']:
+            ip, port = peer
+
+            print(f'{ip}:{port}')
+    elif args.command == 'handshake':
+        with open(args.filename, 'rb') as f:
+            torrent = Torrent.load(f)
+
+        ip, port = args.address.split(':', maxsplit=2)
+        address = (ip, int(port))
+
+        with Peer(MY_PEER_ID, torrent, address) as peer:
+            handshake = peer.handshake()
+
+        if handshake:
+            print(f'Peer ID: {handshake.peer_id.hex()}')
+    elif args.command == 'download_piece':
+        with open(args.filename, 'rb') as f:
+            torrent = Torrent.load(f)
+
+        tracker = Tracker(MY_PEER_ID, torrent)
+
+        address = random.choice(tracker.request()['peers'])
+
+        with Peer(MY_PEER_ID, torrent, address) as peer:
+            piece_data = peer.download_piece(args.piece_index)
+
+        if isinstance(piece_data, bytearray):
+            with open(args.output, 'wb') as f:
+                f.write(piece_data)
+
+            print(f'Piece {args.piece_index} downloaded to {args.output}.')
+
+
+if __name__ == '__main__':
     main()
